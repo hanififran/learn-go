a053101cc4e9719cfe35c13c7690bf770e124e0d
hanififran <hanif.antoni@escolifesciences.com>
2025-05-16 11:08:31 +0700

    routing using gorilla/mux

--- hello.go
+++ hello.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	
+	"github.com/gorilla/mux"
 )
 
 func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my website")
-	})
+	r := mux.NewRouter()
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	page := vars["page"]
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.ListenAndServe(":8080", nil)
+	fmt.Fprintf(w, "You've requested the book: %s on the page %s\n", title, page)
+})
+	http.ListenAndServe(":8080",r)
 }
