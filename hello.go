8c06b78525622d744fe0ac9cd2adfe5613705eec
hanififran <hanif.antoni@escolifesciences.com>
2025-05-16 11:21:59 +0700

    using functional routing

--- hello.go
+++ hello.go
@@ -7,14 +7,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func main(){
-	r := mux.NewRouter()
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+func getBook(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r)
 	title := vars["title"]
 	page := vars["page"]
-
+	
 	fmt.Fprintf(w, "You've requested the book: %s on the page %s\n", title, page)
-})
+}
+func main(){
+	r := mux.NewRouter()
+	r.HandleFunc("/books/{title}/page/{page}", getBook).Methods("GET")
+
 	http.ListenAndServe(":8080",r)
 }
